dd0ccefd72a025732436825b52cd5813e64546c6
sin woo kyoung <sinwookyoung15@gmail.com>
2025-01-19 20:44:38 +0900

    test2

--- calendar_scheduler/lib/screen/home_screen.dart
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -71,7 +71,7 @@ class _HomeScreenState extends State<HomeScreen> {
           selectedDayPredicate: (DateTime date) {
             if (selectedDay == null) {
               return false;
-            }
+            } 
 
             return date.isAtSameMomentAs(selectedDay!);
           },
